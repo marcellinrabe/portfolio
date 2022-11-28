7e0e041d92453f3e44eb88f3342e2d4dbd778d4d
marcellinrabe <marcellinp20.aps1a@gmail.com>
2022-11-28 11:22:36 +0300

    [PRESENTABLE] works field
    [A FAIRE]
    For each project on hover, show description inside upon card image

--- src/Components/Works/index.jsx
+++ src/Components/Works/index.jsx
@@ -1,9 +1,68 @@
+import Keyboard from '../../assets/keyboard-light.gif';
+import Line from '../App/Line';
+import Card from './Card';
+
+const cards = [
+    {
+        key: 1,
+        project: 'Project 1',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quas fugit saepe iure ullam quaerat omnis, quos accusantium nemo
+         quae quis.`,
+        github_link: 'https://www.github.com/marcellinrabe/didy-folo',
+        img: {
+            src: Keyboard,
+            alt: 'didy folo web view',
+        },
+    },
+    {
+        key: 2,
+        project: 'Project 2',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Non perferendis, ducimus, exercitationem cumque obcaecati facere 
+         totam quasi dignissimos, ut cum perspiciatis sunt aliquid impedit
+          rem architecto autem eos inventore laboriosam!`,
+        github_link: 'https://www.github.com/marcellinrabe/api-didy-folo',
+        img: {
+            src: Keyboard,
+            alt: 'didy folo web view',
+        },
+    },
+    {
+        key: 3,
+        project: 'Project 3',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         eum inventore soluta laborum ipsa dolore sapiente fugiat sed?
+           eos deleniti dignissimos ipsa.`,
+        github_link: 'https://www.github.com/marcellinrabe/api-didy-folo',
+        img: {
+            src: Keyboard,
+            alt: 'didy folo web view',
+        },
+    },
+];
 export default function Works() {
     return (
-        <div id="works">
-            <div className="vh-100 w-100 d-flex align-items-center justify-content-center">
-                Réalisations
+        <div id="works" className="vh-100 w-75 m-auto">
+            <div className="grid grid-cols-3 gap-16">
+                {cards.map(
+                    ({ key, project, desc, github_link, img }, index) => (
+                        <Card
+                            key={key + index}
+                            project={project}
+                            desc={desc}
+                            github_link={github_link}
+                            img={img}
+                        />
+                    )
+                )}
             </div>
+            <div className="text-center my-5">
+                <button className="bg-dark text-white p-2 rounded-1">
+                    Voir tout sur github
+                </button>
+            </div>
+            <Line />
         </div>
     );
 }
